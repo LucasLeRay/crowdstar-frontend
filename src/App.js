69599927bbc96d1c3ee280619056345297efa450
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-04-01 16:36:57 -0700

    feat: add fetch room on feed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <CreateBoard />
         </Route>
 
-        <Route path="/feed/:id">
+        <Route path="/feed/:room">
           <Feed />
         </Route>
 
--- src/Feed/index.js
+++ src/Feed/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from 'react'
+import { useParams } from 'react-router-dom'
 
+import apiRequest from '../commons/helpers/apiRequest'
 import Thermometer from '../commons/Components/Thermometer'
 
+async function getRoomInformation(room) {
+    try {
+        const board = await apiRequest(
+          undefined,
+          'GET',
+          `board/${room}`,
+        )
+        return (board);
+      } catch (err) {
+        console.error(err)
+        return (err);
+      }
+} 
+
 function Feed() {
-    return (
-        <div>
-            <Thermometer current="60" max="1000"/>
-        </div>
-    )
+  let { room } = useParams()
+
+  getRoomInformation(room)
+
+  return (
+    <div>
+      <Thermometer current="60" max="1000" />
+    </div>
+  )
 }
 
-export default Feed;
+export default Feed
